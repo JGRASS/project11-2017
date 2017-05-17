71802eac24e6a0a72bb28fb9d6d8f502b5ead3c1
Mire96 <milos.m96@hotmail.com>
2017-05-17 17:34:13 +0200

    Uradjen javadoc za klasu predmet

--- My Study Life/src/predmeti/Predmet.java
+++ My Study Life/src/predmeti/Predmet.java
@@ -50,101 +50,171 @@ public class Predmet implements Serializable {
 		setSkolskaGodina(skolskaGodina);
 	}
 	/**
-	 * Vraca link za FONForum
+	 * Vraca link za sajt FONForum
 	 * @return
 	 */
 	public String getForum() {
 		return forum;
 	}
-
+	/**
+	 * Postavlja novi link za sajt FONForum
+	 * @param Link koji zelimo da postavimo
+	 */
 	public void setForum(String forum) {
 		if(forum==null) throw new RuntimeException("Link za forum mora biti pravilno unet"); 
 		this.forum = forum;
 	}
-
+	/**
+	 * Vraca link za sajt Puskice
+	 * @return
+	 */
 	public String getPuskice() {
 		return puskice;
 	}
-
+	/**
+	 * Postavlja novi link za sajt Puskice
+	 * @param Link koji zelimo da postavimo
+	 */
 	public void setPuskice(String puskice) {
 		if(puskice==null) throw new RuntimeException("Link za puskice mora biti ispravno unet");
 		this.puskice = puskice;
 	}
 
-
+	/**
+	 * Vraca naziv predmeta
+	 * @return
+	 */
 	public String getNaziv() {
 		return naziv;
 	}
-
+	/**
+	 * Postavlja novi naziv za predmet
+	 * @param Naziv koji zelimo da postavimo
+	 */
 	public void setNaziv(String naziv) {
 		if(naziv==null || naziv.length()<2)
 			throw new RuntimeException("Naziv predeta mora biti ispravno unet");
 		this.naziv = naziv;
 	}
-
+	/**
+	 * Vraca broj ESBP poena koji ovaj predmet nosi
+	 * @return
+	 */
 	public int getESBP() {
 		return ESBP;
 	}
-
+	/**
+	 * Postavlja novu vrednost ESBP poena za ovaj predmet
+	 * @param Vrednost ESBP poena
+	 */
 	public void setESBP(int ESBP) {
 		if(ESBP<1 || ESBP>30)
 			throw new RuntimeException("Pogresno uneti ESBP bodovi");
 		this.ESBP = ESBP;
 	}
-
+	/**
+	 * Vraca u kojoj skolskoj godini se pohadja predmet
+	 * @return
+	 */
 	public String getSkolskaGodina() {
 		return skolskaGodina;
 	}
-
+	/**
+	 * Postavlja novu vrednost za skolsku godinu u kojoj se polaze predmet
+	 * @param Vrednost za godinu koju zelimo da postavimo
+	 */
 	public void setSkolskaGodina(String skolskaGodina) {
 		if(skolskaGodina==null || skolskaGodina.length()!=9 || !skolskaGodina.contains("/"))	
 			throw new RuntimeException("Skolska goidna mora biti uneta ispravno");
 		this.skolskaGodina = skolskaGodina;
 	}
-	
+	/**
+	 * Vraca u kom semestru se pohadja predmet
+	 * @return
+	 */
 	public int getSemestar() {
 		return semestar;
 	}
-	
+	/**
+	 * Postavlja semestar u kom se predmet pohadja 
+	 * @param Broj semestra koji zelimo da postavimo
+	 */
 	public void setSemestar(int semestar) {
 		if(semestar<1 || semestar>12)
 			throw new RuntimeException("Pogresno unet semestar");
 		this.semestar = semestar;
 	}
-	
+	/**
+	 * Proverava da li je ovaj predmet polozen
+	 * @return 
+	 */
 	public boolean isPolozen() {
 		return polozen;
 	}
-
+	
+	/**
+	 * Postavlja vrednost atributa koji odredjuje da li je predmet polozen ili ne.
+	 * @param True ili false u zavisnosti da li je predmet polozen ili ne.
+	 */
 	public void setPolozen(boolean polozen) {
 		this.polozen = polozen;
 	}
-
+	
+	/**
+	 * Vraca vrednost ocene ako je predmet polozen
+	 * @return
+	 */
 	public int getOcena() {
 		return ocena;
 	}
-
+	
+	/**
+	 * Postavlja vrednost ocene.
+	 * @param Zeljena ocena
+	 */
 	public void setOcena(int ocena) {
 		if(ocena<5 || ocena>10)
 			throw new RuntimeException("Pogresno uneta ocena");
 		this.ocena = ocena;
 	}
-
+	
+	/**
+	 * Vraca napomenu o ovom predmetu
+	 * @return
+	 */
 	public String getNapomena() {
 		return napomena;
 	}
-
+	
+	/**
+	 * Zapisuje novu napomenu za predmetu
+	 * @param Napomena u string formatu
+	 */
 	public void setNapomena(String napomena) {
 		this.napomena = napomena;
 	}
 	
+	/**
+	 * Proverava da li je predmet jednosemestralan.
+	 * @return
+	 */
 	public boolean getJednosemestralan() {
 		return jednosemestralan;
 	}
-
+	
+	/**
+	 * Postavlja da li je predmet jednosemestralan ili dvosemestralan.
+	 * @param Postavljanjem ovog parametra na true, predmet se smatra jednosemestralnim, ako je false onda je dvosemestralan
+	 */
 	public void setJednosemestralan(boolean jednosemestralan) {
 		this.jednosemestralan = jednosemestralan;
 	}
+	
+	/**
+	 * Preradjena equals metoda, proverava da li su naziv, ESBP poeni i skolska godina jednaki, ako jesu vraca true.
+	 * u suprotnom false.
+	 * @param Predmet sa kojim poredimo ovaj predmet.
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		Predmet p = (Predmet)obj;
